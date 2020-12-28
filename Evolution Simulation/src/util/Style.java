a00f5699e37b0e8659aa82899b3dd996aff68ba6
Angelo <Angelo@JELLO-LAPTOP>
2020-12-28 16:57:06 -0500

    Created a world for the organisms to live in

--- Evolution Simulation/src/util/Style.java
+++ Evolution Simulation/src/util/Style.java
@@ -5,5 +5,10 @@ import java.awt.Color;
 public class Style {
 
 	public static Color levelBackground = Color.getHSBColor(0.7f, 0.1f, 0.3f);
-
+	
+	public static Color seaColor = Color.getHSBColor(0.55f, 0.3f, 0.2f);
+	public static Color shallowsColor = Color.getHSBColor(0.55f, 0.3f, 0.4f);
+	public static Color landColor = Color.getHSBColor(0.31f, 0.3f, 0.5f);
+	public static Color hillsColor = Color.getHSBColor(0.31f, 0.3f, 0.4f);
+	
 }
